9008df867234ee46e547d5a06dbed1f2b2be15cf
Roman Kozlovskyi <krzroman@gmail.com>
2013-02-22 12:00:27 +0200

    make min

--- static/js/backbone.analytics-min.js
+++ static/js/backbone.analytics-min.js
@@ -1,5 +1,1 @@
-// Created by Kendall Buchanan, (https://github.com/kendagriff)
-// Modified by Paul English, (https://github.com/nrub)
-// MIT licence
-// Version 0.0.2
-(function(){var e=Backbone.History.prototype.loadUrl;Backbone.History.prototype.loadUrl=function(t){var n=e.apply(this,arguments),r=this.fragment=this.getFragment(t);return/^\//.test(r)||(r="/"+r),window._gaq!==undefined&&window._gaq.push(["_trackPageview",r]),n}}).call(this);
+(function(){var _loadUrl=Backbone.History.prototype.loadUrl;Backbone.History.prototype.loadUrl=function(fragmentOverride){var matched=_loadUrl.apply(this,arguments),fragment=this.fragment=this.getFragment(fragmentOverride);if(!/^\//.test(fragment))fragment="/"+fragment;if(window._gaq!==undefined)window._gaq.push(["_trackPageview",fragment]);return matched}}).call(this);
