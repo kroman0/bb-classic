9ed9d296d7187878d42dc21811e645c869122920
Roman Kozlovskyi <krzroman@gmail.com>
2013-02-28 08:49:59 +0200

    jshint

--- static/js/general.js
+++ static/js/general.js
@@ -42,9 +42,7 @@ var add_hash = function () {
         var rs = /^[#\/]|\s+$/g;
         var rr = _.map(_.filter(_.keys(workspace.routes),function(i){return i.indexOf("*")===-1;}),function(i){return workspace._routeToRegExp(i);});
         var cur_hashs = _.uniq(_.map($("a"), function (i) {return i.hash.replace(rs,'');}));
-        var inroutes = function(routes, hash) {
-            return _.every(routes,function(i){return !i.test(hash);});
-        };
+        var inroutes = function(routes, hash) {return _.every(routes,function(i){return !i.test(hash);});};
         while ((h = cur_hashs.pop())) {
             if (inroutes(rr,h)) {
                 if (uniq_hash.indexOf(h) == -1) {
