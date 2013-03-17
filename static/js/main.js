b525d713b167f6de4499f912e99ca810594fa873
Roman Kozlovskyi <krzroman@gmail.com>
2013-03-17 21:23:26 +0200

    Start experiments with marionette.js

--- static/js/main.js
+++ static/js/main.js
@@ -219,12 +219,89 @@ $(function () {
             trigger: true
         });
     });
-    views.navbar = new NavView({
-        model: models.mydata,
-        el: '.navbar'
-    }).render();
-    models.mydata.once("sync", function () {
-        Backbone.history.start();
+//     views.navbar = new NavView({
+//         model: models.mydata,
+//         el: '.navbar'
+//     }).render();
+//     models.mydata.once("sync", function () {
+//         Backbone.history.start();
+//     });
+//     models.mydata.fetch();
+});
+
+
+
+
+
+
+BB = new Backbone.Marionette.Application();
+
+BB.addRegions({
+    navRegion: ".navbar",
+    mainRegion: "#content"
+});
+
+AngryCat = Backbone.Model.extend({});
+
+AngryCats = Backbone.Collection.extend({
+    model: AngryCat
+});
+
+AngryCatView = Backbone.Marionette.ItemView.extend({
+    template: "#angry_cat-template",
+    tagName: 'tr',
+    className: 'angry_cat'
+});
+
+AngryCatsView = Backbone.Marionette.CompositeView.extend({
+  tagName: "table",
+  id: "angry_cats",
+  className: "table-striped table-bordered",
+  template: "#angry_cats-template",
+  itemView: AngryCatView,
+
+  appendHtml: function(collectionView, itemView){
+    collectionView.$("tbody").append(itemView.el);
+  }
+});
+
+NavBarView = Backbone.Marionette.View.extend({
+    template: "#nav-template",
+    className: "navbar-inner",
+    initialize: function () {
+        this.model.bind("change", this.render, this);
+    }
+});
+
+
+BB.addInitializer(function(options) {
+    var angryCatsView = new AngryCatsView({
+        collection: options.cats
     });
-    models.mydata.fetch();
+    var navbarView = new NavBarView({
+        model: new MyModel()
+    });
+    BB.navRegion.show(navbarView);
+    BB.mainRegion.show(angryCatsView);
+    navbarView.model.fetch();
+});
+
+$(document).ready(function() {
+    // Here we create a bunch of models at the same time as the collection.
+    var cats = new AngryCats([
+        new AngryCat({
+        name: 'Wet Cat'
+    }),
+        new AngryCat({
+        name: 'Bitey Cat'
+    }),
+        new AngryCat({
+        name: 'Surprised Cat'
+    })
+        ]);
+
+
+    BB.start({
+        cats: cats
+    });
 });
