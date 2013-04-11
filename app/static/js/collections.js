335ca7f20f3d006bb2ff8999564b353c38557b0c
Roman Kozlovskyi <krzroman@gmail.com>
2013-04-11 17:28:02 +0300

    jslint #3

--- app/static/js/collections.js
+++ app/static/js/collections.js
@@ -1,106 +1,119 @@
-var Projects = Backbone.Collection.extend({
-    url: '/api/projects.xml',
-    model: Project
-});
-var Companies = Backbone.Collection.extend({
-    url: '/api/companies.xml',
-    model: Company
-});
-var People = Backbone.Collection.extend({
-    parent_id: null, // project id
-    url: function () {
-        if (this.parent_id) return '/api/projects/' + this.parent_id + '/people.xml';
-        return '/api/people.xml';
-    },
-    model: Person
-});
-var Posts = Backbone.Collection.extend({
-    parent_id: null, // project id
-    url: function () {
-        return '/api/projects/' + this.parent_id + '/posts.xml';
-    },
-    model: Post
-});
-var Attachments = Backbone.PageableCollection.extend({
-    mode: 'client',
-    parent_id: null, // project id
-    url: function () {
-        return '/api/projects/' + this.parent_id + '/attachments.xml';
-    },
-    model: Attachment
-});
-var Calendar = Backbone.Collection.extend({
-    parent_id: null, // project id
-    url: function () {
-        return '/api/projects/' + this.parent_id + '/calendar_entries.xml';
-    },
-    model: CalendarEntry
-});
-var Categories = Backbone.PageableCollection.extend({
-    mode: 'client',
-    parent_id: null, // project id
-    url: function () {
-        return '/api/projects/' + this.parent_id + '/categories.xml';
-    },
-    model: Category
-});
-var TimeEntries = Backbone.PageableCollection.extend({
-    mode: 'client',
-    parent_id: null, // project id
-    parent: 'projects',
-    filter_report: null, // report filter
-    //         This action accepts the following query parameters:
-    //         `from`, `to`, `subject_id`, `todo_item_id`,
-    //         `filter_project_id`, and `filter_company_id`. 
-    //         Both `from` and `to` should be dates in `YYYYMMDD` format, and can be used to restrict the result to a particular date range.
-    //         The `subject_id` parameter lets you constrain the result to a single person’s time entries.
-    //         `todo_item_id` restricts the result to only those entries relating to the given todo item.
-    //         `filter_project_id` restricts the entries to those for the given project,
-    //         and `filter_company_id` restricts the entries to those for the given company.
-    url: function () {
-        if (this.parent_id) return '/api/' + this.parent + '/' + this.parent_id + '/time_entries.xml';
-        if (this.filter_report) return '/api/time_entries/report.xml?' + this.filter_report;
-        return '/api/time_entries/report.xml';
-    },
-    model: TimeEntry
-});
-var TodoTimeEntries = TimeEntries.extend({
-    parent: 'todo_items'
-});
-var TodoItems = Backbone.Collection.extend({
-    parent_id: null,
-    url: function () {
-        return '/api/todo_lists/' + this.parent_id + '/todo_items.xml';
-    },
-    model: TodoItem
-});
-var TodoLists = Backbone.Collection.extend({
-    responsible_party: null, // person id
-    parent_id: null, // project id
-    filter_status: null, // filter for project [all\pending\finished]
-    url: function () {
-        if (this.parent_id && this.filter_status) return '/api/projects/' + this.parent_id + '/todo_lists.xml?filter=' + this.filter_status;
-        if (this.parent_id) return '/api/projects/' + this.parent_id + '/todo_lists.xml';
-        if (this.responsible_party === null) return '/api/todo_lists.xml';
-        if (this.responsible_party === "") return '/api/todo_lists.xml?responsible_party=';
-        return '/api/todo_lists.xml?responsible_party=' + this.responsible_party;
-    },
-    model: TodoList
-});
-var Comments = Backbone.Collection.extend({
-    parent_id: null, // parent id
-    parent_type: null, // posts|milestones|todo_items
-    url: function () {
-        return '/api/' + this.parent_type + '/' + this.parent_id + '/comments.xml';
-    },
-    model: Comment
-});
-var PostComments = Comments.extend({
-    parent_type: 'posts'
-});
-var TodoComments = Comments.extend({
-    parent_type: 'todo_items'
-});
-var CalendarEntryComments = Comments.extend({
-    parent_type: 'milestones'
-});
+/*jslint nomen: true*/
+/*global window, $, _, Backbone*/
+(function () {
+    "use strict";
+    window.Projects = Backbone.Collection.extend({
+        url: '/api/projects.xml',
+        model: window.Project
+    });
+    window.Companies = Backbone.Collection.extend({
+        url: '/api/companies.xml',
+        model: window.Company
+    });
+    window.People = Backbone.Collection.extend({
+        parent_id: null, // project id
+        url: function () {
+            return this.parent_id ? '/api/projects/' + this.parent_id + '/people.xml' : '/api/people.xml';
+        },
+        model: window.Person
+    });
+    window.Posts = Backbone.Collection.extend({
+        parent_id: null, // project id
+        url: function () {
+            return '/api/projects/' + this.parent_id + '/posts.xml';
+        },
+        model: window.Post
+    });
+    window.Attachments = Backbone.PageableCollection.extend({
+        mode: 'client',
+        parent_id: null, // project id
+        url: function () {
+            return '/api/projects/' + this.parent_id + '/attachments.xml';
+        },
+        model: window.Attachment
+    });
+    window.Calendar = Backbone.Collection.extend({
+        parent_id: null, // project id
+        url: function () {
+            return '/api/projects/' + this.parent_id + '/calendar_entries.xml';
+        },
+        model: window.CalendarEntry
+    });
+    window.Categories = Backbone.PageableCollection.extend({
+        mode: 'client',
+        parent_id: null, // project id
+        url: function () {
+            return '/api/projects/' + this.parent_id + '/categories.xml';
+        },
+        model: window.Category
+    });
+    window.TimeEntries = Backbone.PageableCollection.extend({
+        mode: 'client',
+        parent_id: null, // project id
+        parent: 'projects',
+        filter_report: null, // report filter
+        //         This action accepts the following query parameters:
+        //         `from`, `to`, `subject_id`, `todo_item_id`,
+        //         `filter_project_id`, and `filter_company_id`.
+        //         Both `from` and `to` should be dates in `YYYYMMDD` format, and can be used to restrict the result to a particular date range.
+        //         The `subject_id` parameter lets you constrain the result to a single person’s time entries.
+        //         `todo_item_id` restricts the result to only those entries relating to the given todo item.
+        //         `filter_project_id` restricts the entries to those for the given project,
+        //         and `filter_company_id` restricts the entries to those for the given company.
+        url: function () {
+            if (this.parent_id) {
+                return '/api/' + this.parent + '/' + this.parent_id + '/time_entries.xml';
+            }
+            if (this.filter_report) {
+                return '/api/time_entries/report.xml?' + this.filter_report;
+            }
+            return '/api/time_entries/report.xml';
+        },
+        model: window.TimeEntry
+    });
+    window.TodoTimeEntries = window.TimeEntries.extend({
+        parent: 'todo_items'
+    });
+    window.TodoItems = Backbone.Collection.extend({
+        parent_id: null,
+        url: function () {
+            return '/api/todo_lists/' + this.parent_id + '/todo_items.xml';
+        },
+        model: window.TodoItem
+    });
+    window.TodoLists = Backbone.Collection.extend({
+        responsible_party: null, // person id
+        parent_id: null, // project id
+        filter_status: null, // filter for project [all\pending\finished]
+        url: function () {
+            if (this.parent_id && this.filter_status) {
+                return '/api/projects/' + this.parent_id + '/todo_lists.xml?filter=' + this.filter_status;
+            }
+            if (this.parent_id) {
+                return '/api/projects/' + this.parent_id + '/todo_lists.xml';
+            }
+            if (this.responsible_party === null) {
+                return '/api/todo_lists.xml';
+            }
+            if (this.responsible_party === "") {
+                return '/api/todo_lists.xml?responsible_party=';
+            }
+            return '/api/todo_lists.xml?responsible_party=' + this.responsible_party;
+        },
+        model: window.TodoList
+    });
+    window.PostComments = Backbone.Collection.extend({
+        parent_id: null, // parent id
+        parent_type: 'posts', // posts|milestones|todo_items
+        url: function () {
+            return '/api/' + this.parent_type + '/' + this.parent_id + '/comments.xml';
+        },
+        model: window.Comment
+    });
+    window.TodoComments = window.PostComments.extend({
+        parent_type: 'todo_items'
+    });
+    window.CalendarEntryComments = window.PostComments.extend({
+        parent_type: 'milestones'
+    });
+}());
