5eb1f2a31489cdefe5968f87a8ec1b9c90cbd70c
Roman Kozlovskyi <krzroman@gmail.com>
2013-05-23 22:19:58 +0300

    gjslint models

--- app/static/js/models.js
+++ app/static/js/models.js
@@ -1,15 +1,15 @@
-/*jslint nomen: true*/
+/*jslint nomen: true, white: true*/
 /*global window, _, Backbone*/
-(function () {
-    "use strict";
-    var urlError = function () {
+(function() {
+    'use strict';
+    var urlError = function() {
             throw new Error('A "url" property or function must be specified');
         },
         BBModel = Backbone.Model.extend({
-            url: function () {
+            url: function() {
                 return this.getUrl();
             },
-            getUrl: function () {
+            getUrl: function() {
                 var base;
                 if (!this.isNew()) {
                     base = _.result(this, 'urlRoot') || _.result(this.collection, 'url') || urlError();
@@ -19,15 +19,15 @@
                 }
                 return base;
             },
-            sync: function (method, model, options) {
+            sync: function(method, model, options) {
                 if (!options.url) {
                     switch (method) {
-                    case "read":
-                    case "create":
+                    case 'read':
+                    case 'create':
                         options.url = _.result(model, 'url');
                         break;
-                    case "update":
-                    case "delete":
+                    case 'update':
+                    case 'delete':
                         options.url = _.result(model, 'getUrl');
                         break;
                     default:
@@ -38,59 +38,59 @@
             }
         });
     window.Project = BBModel.extend({
-        urlRoot: "/api/projects/",
-        icon: function () {
+        urlRoot: '/api/projects/',
+        icon: function() {
             switch (this.get('status')) {
-            case "active":
-                return "icon-play";
-            case "archived":
-                return "icon-stop";
-            case "on_hold":
-                return "icon-pause";
+            case 'active':
+                return 'icon-play';
+            case 'archived':
+                return 'icon-stop';
+            case 'on_hold':
+                return 'icon-pause';
             }
         }
     });
     window.Company = BBModel.extend({
-        urlRoot: "/api/companies/"
+        urlRoot: '/api/companies/'
     });
     window.Person = BBModel.extend({
-        urlRoot: "/api/people/",
-        name: function () {
+        urlRoot: '/api/people/',
+        name: function() {
             return this.get('first-name') + ' ' + this.get('last-name');
         }
     });
     window.Post = BBModel.extend({
-        urlRoot: "/api/posts/"
+        urlRoot: '/api/posts/'
     });
     window.Attachment = BBModel.extend();
     window.CalendarEntry = BBModel.extend({
-        urlRoot: "/api/projects/#{project_id}/calendar_entries/"
+        urlRoot: '/api/projects/#{project_id}/calendar_entries/'
     });
     window.Category = BBModel.extend({
-        urlRoot: "/api/categories/"
+        urlRoot: '/api/categories/'
     });
     window.TimeEntry = BBModel.extend({
-        urlRoot: "/api/time_entries/"
+        urlRoot: '/api/time_entries/'
     });
     window.TodoItem = BBModel.extend({
-        urlRoot: "/api/todo_items/",
-        complete: function () {
+        urlRoot: '/api/todo_items/',
+        complete: function() {
             this.save('completed', true, {url: _.result(this, 'url').replace('.xml', '/complete.xml')});
         },
-        uncomplete: function () {
+        uncomplete: function() {
             this.save('completed', false, {url: _.result(this, 'url').replace('.xml', '/uncomplete.xml')});
         }
     });
     window.TodoList = BBModel.extend({
-        urlRoot: "/api/todo_lists/"
+        urlRoot: '/api/todo_lists/'
     });
     window.Comment = BBModel.extend({
-        urlRoot: "/api/comments/"
+        urlRoot: '/api/comments/'
     });
     window.MyModel = window.Person.extend({
         defaults: {
             id: null
         },
-        url: "/api/me.xml"
+        url: '/api/me.xml'
     });
 }());
