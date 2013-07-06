1ca2dd7f0015f49c709ddd2ab04c5e17d35e14f4
Roman Kozlovskyi <krzroman@gmail.com>
2013-07-06 15:35:44 +0300

    form-horizontal for todo

--- app/static/js/templates.js
+++ app/static/js/templates.js
@@ -811,34 +811,66 @@ templates['#todo'] = '<% var prid=view.model.id; var tdlid=item.get("todo-list-i
 '<i class="todo icon-pencil" data-id="<%- item.id %>"></i>' +
 '<% if (!_.isFinite(view.todo_item)) { %><i class="todo icon-trash" data-id="<%- item.id %>"></i><% } %>';
 templates['#todoedit'] = '<% var pp=view.options.collections.project_people.get_or_create(view.model.id); %>' +
-'<div class="edit_todo_wrapper"><form class="edit_todo">' +
-'<label for="todoContent<%- item.id %>">Todo content</label>' +
+'<div class="edit_todo_wrapper"><form class="edit_todo form-horizontal">' +
+'<div class="control-group">' +
+'<label class="control-label" for="todoContent<%- item.id %>">Todo content</label>' +
+'<div class="controls">' +
 '<textarea id="todoContent<%- item.id %>" name="content" required><%= item.get("content") %></textarea>' +
-'<label for="todoDueAt<%- item.id %>">Due date</label>' +
-'<input id="todoDueAt<%- item.id %>" data-provide="datepicker" data-date-autoclose="true" data-date-format="yyyy-mm-dd" type="text" class="input-small" name="due-at" placeholder="YYYY-MM-DD" value="<%= item.get("due-at") %>"><br />' +
-'<label for="responsiblePerson<%- item.id %>">Responsible person</label>' +
+'</div></div>' +
+'<div class="control-group">' +
+'<label class="control-label" for="todoDueAt<%- item.id %>">Due date</label>' +
+'<div class="controls">' +
+'<input id="todoDueAt<%- item.id %>" data-provide="datepicker" data-date-autoclose="true" data-date-format="yyyy-mm-dd" type="text" class="input-small" name="due-at" placeholder="YYYY-MM-DD" value="<%= item.get("due-at") %>">' +
+'</div></div>' +
+'<div class="control-group">' +
+'<label class="control-label" for="responsiblePerson<%- item.id %>">Responsible person</label>' +
+'<div class="controls">' +
 '<select  id="responsiblePerson<%- item.id %>" name="responsible-party">' +
 '<option value="">Nobody</option>' +
 '<% pp.each(function (i) { %><option value="<%- i.id %>" <% if (i.id==item.get("responsible-party-id")) { %>selected="selected"<% } %>><%- i.name() %></option><% }) %>' +
 '</select>' +
-'<div class="checkbox"><label><input type="checkbox" name="notify" value="true"> Notify responsible person</label></div>' +
+'</div></div>' +
+'<div class="control-group">' +
+'<label class="control-label" for="notify<%- item.id %>">Notify responsible person</label>' +
+'<div class="controls">' +
+'<input id="notify<%- item.id %>" type="checkbox" name="notify" value="true">' +
+'</div></div>' +
+'<div class="control-group">' +
+'<div class="controls">' +
 '<button data-id="<%- item.id %>" class="btn btn-default save" title="Save"><i class="icon-ok"></i></button>' +
 '<button data-id="<%- item.id %>" class="btn btn-default reset" title="Cancel"><i class="icon-off"></i></button>' +
+'</div></div>' +
 '</form></div>';
 templates['#todoadd'] = '<% var pp=view.options.collections.project_people.get_or_create(view.model.id); %>' +
 '<dd><button type="button" class="btn" data-toggle="collapse" data-target=".add_todo_wrapper">Add an item</button>' +
-'<div class="add_todo_wrapper collapse"><form class="add_todo">' +
-'<label for="todoContent">Todo content</label>' +
+'<div class="add_todo_wrapper collapse"><form class="add_todo form-horizontal">' +
+'<div class="control-group">' +
+'<label class="control-label" for="todoContent">Todo content</label>' +
+'<div class="controls">' +
 '<textarea id="todoContent" name="content" required></textarea>' +
-'<label for="todoDueAt">Due date</label>' +
-'<input id="todoDueAt" data-provide="datepicker" data-date-autoclose="true" data-date-format="yyyy-mm-dd" type="text" class="input-small" name="due-at" placeholder="YYYY-MM-DD" value=""><br />' +
-'<label for="responsiblePerson">Responsible person</label>' +
+'</div></div>' +
+'<div class="control-group">' +
+'<label class="control-label" for="todoDueAt">Due date</label>' +
+'<div class="controls">' +
+'<input id="todoDueAt" data-provide="datepicker" data-date-autoclose="true" data-date-format="yyyy-mm-dd" type="text" class="input-small" name="due-at" placeholder="YYYY-MM-DD" value="">' +
+'</div></div>' +
+'<div class="control-group">' +
+'<label class="control-label" for="responsiblePerson">Responsible person</label>' +
+'<div class="controls">' +
 '<select  id="responsiblePerson" name="responsible-party">' +
 '<option value="">Nobody</option>' +
 '<% pp.each(function (i) { %><option value="<%- i.id %>"><%- i.name() %></option><% }) %>' +
 '</select>' +
-'<div class="checkbox"><label><input type="checkbox" name="notify" value="true"> Notify responsible person</label></div>' +
+'</div></div>' +
+'<div class="control-group">' +
+'<label class="control-label" for="notify">Notify responsible person</label>' +
+'<div class="controls">' +
+'<input id="notify" type="checkbox" name="notify" value="true">' +
+'</div></div>' +
+'<div class="control-group">' +
+'<div class="controls">' +
 '<button class="btn btn-default add" title="Add"><i class="icon-plus"></i></button>' +
+'</div></div>' +
 '</form></div></dd>';
 templates['#project-todo-list'] = '<%= view.block("#header") %>' +
 '<%= view.block("#project-nav") %>' +
